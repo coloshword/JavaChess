fc890f6eb55b385476b7ea2abadc88298ebdcbb4
coloshword <aceliang2001@gmail.com>
2021-06-15 17:33:00 -0400

    added a system to track chess pieces

--- Tile.java
+++ Tile.java
@@ -9,6 +9,8 @@ public class Tile extends JPanel{
     int position;
     // constructor
     // int x and y correspond to the coordinates, int a to the position
+    // the 2d array to keep positions
+    char[][] boardInfo = new char[8][8];
     public Tile(int a, int x, int y) {
         // rgbs of the two colors:
         // white
@@ -25,5 +27,34 @@ public class Tile extends JPanel{
             this.setBackground(new Color(rgbPurple[0], rgbPurple[1], rgbPurple[2]));
         }
         this.setFocusable(true);
+        this.defineBoardArray();
     }
+    
+    //letter will represent the piece, capital means white, lowercase means black
+    public void defineBoardArray() {
+        boardInfo[0][0] = 'r';
+        boardInfo[0][1] = 'k';
+        boardInfo[0][2] = 'b';
+        boardInfo[0][3] = 'q';
+        boardInfo[0][4] = 'k';
+        boardInfo[0][5] = 'b';
+        boardInfo[0][6] = 'k';
+        boardInfo[0][7] = 'r';
+        for(int i = 0; i < 8; i++) {
+            boardInfo[1][i] = 'p';
+        }
+
+        boardInfo[7][0] = 'R';
+        boardInfo[7][1] = 'K';
+        boardInfo[7][2] = 'B';
+        boardInfo[7][3] = 'Q';
+        boardInfo[7][4] = 'K';
+        boardInfo[7][5] = 'B';
+        boardInfo[7][6] = 'K';
+        boardInfo[7][7] = 'R';
+        for(int i = 0; i < 8; i++) {
+            boardInfo[1][i] = 'P';
+        }
+    }
+    
 }
